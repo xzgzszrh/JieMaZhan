817fa078713dc261c94c9ca5c5d3de860a7dd621
Oranges-sour <873516725@qq.com>
2026-02-16 11:53:33 +0800

    123

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
